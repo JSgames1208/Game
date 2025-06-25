9e5e6da7082aa816c303cfe05797a75e3eb410c3
JSgames1208 <soetejasper@gmail.com>
2025-06-25 22:54:47 +0200

    Finished block placing and saving, Started on hotbar.

--- src/shared/network.lua
+++ src/shared/network.lua
@@ -2,6 +2,11 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ByteNet = require(ReplicatedStorage.Packages.Bytenet)
 
 local packets = ByteNet.defineNamespace("IslandNet", function()
+	local SlotStruct = ByteNet.struct({
+		count = ByteNet.int32,
+		id = ByteNet.string,
+	})
+
 	return {
 		PlaceBlock = ByteNet.definePacket({
 			-- Use `value`, not `structure` or dataTypes
@@ -12,6 +17,11 @@ local packets = ByteNet.defineNamespace("IslandNet", function()
 				blockId = ByteNet.string,
 			}),
 			reliabilityType = "reliable", -- ensure the block place request always reaches the server
+		}),
+		InventorySync = ByteNet.definePacket({
+			value = ByteNet.array(SlotStruct),
+
+			reliabilityType = "reliable",
 		}),
 	}
 end)
